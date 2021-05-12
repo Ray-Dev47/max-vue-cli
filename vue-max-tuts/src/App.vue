faae81801afa7c9ad80e38afe131f05a3e8a1af5
Raymond Idengeli <ray.idengeli@gmail.com>
2021-05-12 19:08:10 +0100

    forms done

--- vue-max-tuts/src/App.vue
+++ vue-max-tuts/src/App.vue
@@ -1,56 +1,153 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <appQuote>
-                    <button v-on:click="selectedComponent = 'appQuote'">Quote</button>
-                    <button v-on:click="selectedComponent = 'appAuthor'">Author</button>
-                    <button v-on:click="selectedComponent = 'appNew'">New</button>
-                    <hr>
-                    <p>{{selectedComponent}}</p>
-                    <!-- dynamic component keyword and tag -->
-                    <keep-alive>
-                         <component :is="selectedComponent">
-                             <p>Default Content</p>
-                         </component>        
-                    </keep-alive>           
-                    <hr>
-                    <h2 slot="">A Quote</h2>  <!-- slot with no name takes default -->
-                    <h1 slot="title">{{ quoteTitle }}</h1>
-                    <!--  slot order adjustmanr -->
-                    <h3 slot="header">A quote to test for multiple slots (should go below)</h3>
-                    <p slot="text">A quote to test for multiple slots (should go above)</p>
-                    
-                </appQuote>
-                
-            </div>
+  <div class="container">
+    <form>
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
+        >
+          <h1>File a Complaint</h1>
+          <hr />
+          <div class="form-group">
+            <label for="email">Mail</label>
+            <input
+              type="text"
+              id="email"
+              class="form-control"
+              v-model.lazy="userData.email"
+            />
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              class="form-control"
+              v-model.lazy="userData.password"
+            />
+          </div>
+          <div class="form-group">
+            <label for="age">Age</label>
+            <input
+              type="number"
+              id="age"
+              class="form-control"
+              v-model.lazy="userData.age"
+            />
+          </div>
         </div>
+      </div>
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group"
+        >
+          <label for="message">Message</label><br />
+          <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+          <textarea
+            id="message"
+            rows="5"
+            class="form-control"
+            v-model="message"
+          ></textarea>
+        </div>
+      </div>
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
+        >
+          <div class="form-group">
+            <label for="sendmail">
+              <input type="checkbox" id="sendmail" value="SendMail" v-model="sendMail" /> Send Mail
+            </label>
+            <label for="sendInfomail">
+              <input type="checkbox" id="sendInfomail" value="SendInfoMail"  v-model="sendMail"/>
+              Send Infomail
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group"
+        >
+          <label for="male">
+            <input type="radio" id="male"  value="Male" v-model="gender" /> Male
+          </label>
+          <label for="female">
+            <input type="radio" id="female"  value="Female" v-model="gender" /> Female
+          </label>
+        </div>
+      </div>
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group"
+        >
+          <label for="priority">Priority</label>
+              <!-- option 1 for default selected priority -->
+          <select id="priority" class="form-control" v-model="selectedPriority">
+              <!-- option 2 for default selected priority -->
+            <option v-for="priority in priorities" v-bind:key="priority" :selected="priority == 'Medium'"> {{ priority }} </option>
+          </select>
+        </div>
+      </div>
+      <hr />
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
+        >
+          <button class="btn btn-primary" v-on:click.prevent="submitted">Submit!</button>
+        </div>
+      </div>
+    </form>
+    <hr />
+    <div class="row" v-if="isSubmitted">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4>Your Data</h4>
+          </div>
+          <div class="panel-body">
+            <p>Mail: {{ userData.email }}</p>
+            <p>Password: {{ userData.password }}</p>
+            <p>Age: {{ userData.age }}</p>
+            <p style="white-space: pre">Message: {{ message }}</p>
+            <p><strong>Send Mail?</strong></p>
+            <ul>
+              <li v-for="item in sendMail" v-bind:key="item"> {{ item }} </li>
+            </ul>
+            <p>Gender: {{ gender }}</p>
+            <p>Priority: {{ selectedPriority }}</p>
+            <p>Switched:</p>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-// import globally
-    import Quotes from './components/Quote';
-    import Author from './components/Author';
-    import New from './components/New';
-
-
-// register globally
-    export default {
-        data: function (){
-            return {
-                quoteTitle: 'A Wonderful Quote',
-                selectedComponent: 'appQuote'
-            }
-        },
-        components: {
-            appQuote: Quotes,
-            appAuthor: Author,
-            appNew: New
-        }
-    }
+export default {
+  data: function() {
+    return {
+      userData: {
+        email: "",
+        password: "",
+        age: 29
+      },
+      message: "A default message",
+      sendMail: [],
+      gender: 'male',
+      priorities: ['High', 'Medium', 'Low'],
+      selectedPriority: "High",
+      isSubmitted : false
+      // email: ''
+    };
+  },
+  methods: {
+      submitted: function(){
+          this.isSubmitted = true
+      }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
