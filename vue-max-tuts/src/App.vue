f008ed05d04024ba4d64965753b196ebd8a13d64
Raymond Idengeli <ray.idengeli@gmail.com>
2021-05-14 10:59:37 +0100

    directives completed

--- vue-max-tuts/src/App.vue
+++ vue-max-tuts/src/App.vue
@@ -1,152 +1,56 @@
 <template>
   <div class="container">
-    <form>
       <div class="row">
-        <div
-          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
-        >
-          <h1>File a Complaint</h1>
-          <hr />
-          <div class="form-group">
-            <label for="email">Mail</label>
-            <input
-              type="text"
-              id="email"
-              class="form-control"
-              v-model.lazy="userData.email"
-            />
-          </div>
-          <div class="form-group">
-            <label for="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              class="form-control"
-              v-model.lazy="userData.password"
-            />
-          </div>
-          <div class="form-group">
-            <label for="age">Age</label>
-            <input
-              type="number"
-              id="age"
-              class="form-control"
-              v-model.lazy="userData.age"
-            />
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div
-          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group"
-        >
-          <label for="message">Message</label><br />
-          <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
-          <textarea
-            id="message"
-            rows="5"
-            class="form-control"
-            v-model="message"
-          ></textarea>
-        </div>
-      </div>
-      <div class="row">
-        <div
-          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
-        >
-          <div class="form-group">
-            <label for="sendmail">
-              <input type="checkbox" id="sendmail" value="SendMail" v-model="sendMail" /> Send Mail
-            </label>
-            <label for="sendInfomail">
-              <input type="checkbox" id="sendInfomail" value="SendInfoMail"  v-model="sendMail"/>
-              Send Infomail
-            </label>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div
-          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group"
-        >
-          <label for="male">
-            <input type="radio" id="male"  value="Male" v-model="gender" /> Male
-          </label>
-          <label for="female">
-            <input type="radio" id="female"  value="Female" v-model="gender" /> Female
-          </label>
-        </div>
-      </div>
-      <div class="row">
-        <div
-          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group"
-        >
-          <label for="priority">Priority</label>
-              <!-- option 1 for default selected priority -->
-          <select id="priority" class="form-control" v-model="selectedPriority">
-              <!-- option 2 for default selected priority -->
-            <option v-for="priority in priorities" v-bind:key="priority" :selected="priority == 'Medium'"> {{ priority }} </option>
-          </select>
-        </div>
-      </div>
-      <hr />
-      <div class="row">
-        <div
-          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
-        >
-          <button class="btn btn-primary" v-on:click.prevent="submitted">Submit!</button>
-        </div>
-      </div>
-    </form>
-    <hr />
-    <div class="row" v-if="isSubmitted">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h4>Your Data</h4>
-          </div>
-          <div class="panel-body">
-            <p>Mail: {{ userData.email }}</p>
-            <p>Password: {{ userData.password }}</p>
-            <p>Age: {{ userData.age }}</p>
-            <p style="white-space: pre">Message: {{ message }}</p>
-            <p><strong>Send Mail?</strong></p>
-            <ul>
-              <li v-for="item in sendMail" v-bind:key="item"> {{ item }} </li>
-            </ul>
-            <p>Gender: {{ gender }}</p>
-            <p>Priority: {{ selectedPriority }}</p>
-            <p>Switched:</p>
-          </div>
-        </div>
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+            <h1>Normal Header </h1>
+            <p v-text="'Some texts'"></p>
+            <p v-html="'<strong>Some bold text </strong>'"></p>
       </div>
     </div>
+    <hr>
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+            <h1>Built in Directives </h1>
+            <p v-changeParagraph:background.delayed="'blue'">Some texts to add color to this paragraph using custom directives</p>
+            <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500}">Some texts to add color to this paragraph using custom directives</p>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
-  data: function() {
-    return {
-      userData: {
-        email: "",
-        password: "",
-        age: 29
-      },
-      message: "A default message",
-      sendMail: [],
-      gender: 'male',
-      priorities: ['High', 'Medium', 'Low'],
-      selectedPriority: "High",
-      isSubmitted : false
-      // email: ''
-    };
-  },
-  methods: {
-      submitted: function(){
-          this.isSubmitted = true
-      }
-  }
+     directives: {
+            'local-highlight': {
+                bind(el, binding, vnode) {
+                    var delay = 0;
+                    if (binding.modifiers['delayed']) {
+                        delay = 3000;
+                    }
+                    if (binding.modifiers['blink']) {
+                        let mainColor = binding.value.mainColor;
+                        let secondColor = binding.value.secondColor;
+                        let currentColor = mainColor;
+                        setTimeout(() => {
+                            setInterval(() => {
+                                currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
+                                if (binding.arg == 'background') {
+                                    el.style.backgroundColor = currentColor;
+                                } else {
+                                    el.style.color = currentColor;
+                                }
+                            }, binding.value.delay);
+                        }, delay);
+                    } else {
+                        setTimeout(() => {
+                            if (binding.arg == 'background') {
+                                el.style.backgroundColor = binding.value.mainColor;
+                            } else {
+                                el.style.color = binding.value.mainColor;
+                            }
+                        }, delay);
+                    }
+                }
+            }
+        }
 };
 </script>
 
--- vue-max-tuts/src/main.js
+++ vue-max-tuts/src/main.js
@@ -3,6 +3,23 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
+Vue.directive('changeParagraph', {
+    bind(el, binding, vnode) {
+        // el.style.backgroundColor = 'green';
+        // el.style.backgroundColor = binding.value;
+        var delay = 0;
+        if (binding.modifiers['delayed']) {
+            delay = 3000;
+        }
+        setTimeout(() => {
+            if (binding.arg == 'background') {
+                el.style.backgroundColor = binding.value;
+            } else {
+                el.style.color = binding.value;
+            }
+        }, delay);
+    }
+});
 
 new Vue({
   el: '#app',
