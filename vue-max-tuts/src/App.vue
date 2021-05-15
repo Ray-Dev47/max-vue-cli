8dcfd6fc5177db0b2094e888fad82fdf160d6c69
Raymond Idengeli <ray.idengeli@gmail.com>
2021-05-15 21:58:16 +0100

    trasition initial

--- vue-max-tuts/src/App.vue
+++ vue-max-tuts/src/App.vue
@@ -1,43 +1,94 @@
 <template>
   <div class="container">
       <div class="row">
-        <h2>Mixings 1</h2>
-        <p>{{ salute }}</p>
-        <button v-on:click="fruits.push('cherry')">Add More</button>
-        <input type="text" class="form-control" v-model="filterText">
-        <ul>
-            <li v-for="fruit in filteredFruits" v-bind:key="fruit"> {{ fruit }} </li>
-        </ul>
-        <hr>
-        <app-list></app-list>
-    </div>   
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+            <h1>Animations</h1>
+            <hr>
+            <button class="btn btn-primary" v-on:click="show = !show">Show Alert</button>
+            <br><br>
+            <transition name="fade">
+                <div class="alert alert-info" v-if="show">This is some alert</div>
+            </transition>
+             <transition name="slide" type="animation">
+                <div class="alert alert-info" v-if="show">This is some alert</div>
+            </transition>
+            <!-- animate in effect -->
+             <transition name="fade" appear>
+                <div class="alert alert-info" v-if="show">This is some alert</div>
+            </transition>
+            
+      </div>
+    </div>
+    <hr>
+   
   </div>
 </template>
 
 <script>
-//importing the list component
-import List from './components/List.vue'
-
-// importing the mixin 
-import { fruitMixin } from "./fruitMixin";
-
 export default {
-    data(){
-        return{
-            salute: 'Hi Ray'
-        }
-
-    },
-    // register the mixin
-    mixins: [fruitMixin],
-
-// register component
-    components: {
-       appList: List
-    },
-    
-    
+     data(){
+         return {
+             show: false
+         }
+     }
+     
 };
 </script>
 
-<style></style>
+<style>
+ .fade-enter{
+     opacity: 0
+
+ }
+
+.fade-enter-active{
+    transition: opacity 1s;
+}
+
+.fade-leave{
+    /* opacity: 1; */
+}
+
+.fade-leave-active{
+    transition: opacity 1s;
+    opacity: 0;
+}
+
+.slide-enter{
+    /* transform: translateY(20px) */
+    opacity: 0;
+}
+
+.slide-enter-active{
+    animation: slide-in 1s ease-out forwards;
+    transition: opacity .5s;
+}
+.slide-leave{
+
+
+}
+.slide-leave-active{
+    animation: slide-out 1s ease-in-out forwards;
+    transition: opacity 1s;
+    opacity: 0;
+}
+
+@keyframes slide-in {
+     from{
+         transform: translateY(20px)
+     }
+     to{
+         transform: translateY(0);
+     }
+}
+
+@keyframes slide-out {
+     from{
+         transform: translateY(0)
+     }
+     to{
+         transform: translateY(20px);
+     }
+}
+
+</style>
--- vue-max-tuts/src/main.js
+++ vue-max-tuts/src/main.js
@@ -2,21 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-// registering filters globally
-Vue.filter('to-uppercase', function(value){
-    return value.toUpperCase()
-})
-
-Vue.filter('to-lowercase', function(value){
-    return value.toLowerCase()
-})
-
-// creating global mixin
-Vue.mixin({
-    created(){
-       console.log ('global mixin created here')
-    }
-})
 
 
 new Vue({
