53f0e3bc2764ff707b8aff0bdecd25f783dc0c53
Raymond Idengeli <ray.idengeli@gmail.com>
2021-05-17 16:35:58 +0100

    http request completed

--- vue-max-tuts/src/App.vue
+++ vue-max-tuts/src/App.vue
@@ -2,239 +2,72 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Animations</h1>
+        <h2>Sending and fetching HTTP Requests</h2>
+        <div class="form-group">
+          <label>Username</label>
+          <input class="form-control" type="text" v-model="user.username" placeholder="Enter fullname" />
+        </div>
+        <div class="form-group">
+          <label>Mail</label>
+          <input class="form-control" type="email" v-model="user.email"  placeholder="Enter email"/>
+        </div>
+        <br />
+        <button class="btn btn-primary" v-on:click="submit">Submit</button>
         <hr />
-        <!-- dynamic names and attributes -->
-        <select v-model="alertAnimation" class="form-control">
-            <option value="fade" >Fade</option>
-            <option value="slide">Slide</option>
-        </select>
-        <hr>
-        <button class="btn btn-primary" v-on:click="show = !show">
-          Show Alert
-        </button>
-        <br /><br />
-        <transition :name="alertAnimation">
-          <div class="alert alert-info" v-if="show">This is some alert</div>
-        </transition>
-        <transition name="slide" type="animation">
-          <div class="alert alert-info" v-if="show">This is some alert</div>
-        </transition>
-        <!-- animate in effect -->
-        <transition name="fade" appear>
-          <div class="alert alert-info" v-if="show">This is some alert</div>
-        </transition>
-        <!-- aninamtion from animate.css by overiding the default classes -->
-        <transition
-          enter-active-class="animate__animated animate__bounce"
-          leave-active-class="animate__animated animate__shakeX"
-        >
-          <div class="alert alert-info" v-if="show">
-            This is some alert to show animation from animate
-          </div>
-        </transition>
-
-        <!-- transitioning between multiple elements  -->
-        <transition :name="alertAnimation" mode="out-in">
-          <div class="alert alert-info" v-if="show" key="info">This is some alert</div>
-          <div class="alert alert-warning" v-else key="warning">This is some Warning</div>
-        </transition>
-        <hr>
-
-        <!-- Understanding JavaScript animation -->
-        <h3>Understanding JavaScript animation</h3>
-        <button class="btn btn-primary" v-on:click="load = !load">Load / Remove Element</button>
-        <br><br>
-        <transition
-             @before-enter="beforeEnter"
-             @enter="enter"
-             @after-enter="afterEnter"
-             @enter-cancelled="enterCancelled"
-
-             @before-leave="beforeLeave"
-             @leave="leave"
-             @after-leave="afterLeave"
-             @leave-cancelled="leaveCancelled"
-             :css="false"
-             >
-            <div style="width: 300px; height: 100px; background-color: lightgreen" v-if="load"></div>
-        </transition>
-        <hr>
-
-
-        <!-- Animating Dynamic Components -->
-        <h3>Animating Dynamic components</h3>
+        <h3>Fetching JSON data</h3>
         <br>
-        <button class="btn btn-primary" v-on:click="selectedComponent == 'app-success-alert' ? selectedComponent = 'app-danger-alert' : selectedComponent = 'app-success-alert'">Toggle Components</button>
+        <button class="btn btn-primary" v-on:click="fetchData">Get Data</button>
         <br> <br>
-        <transition name="fade" mode="out-in">
-            <component :is="selectedComponent"></component>
-        </transition>
-        <hr>
-
-        <!--ANIMATING LISTS WITH TRANSITION-GROUP -->
-        <h3>ANIMATING LISTS WITH TRANSITION-GROUP</h3>
-        <button class="btn btn-primary" @click="addItem">Add Item</button>
-        <hr>
-        <br>
         <ul class="list-group">
-            <transition-group name="slide">
-                 <li 
-                    class="list-group-item" 
-                    v-for="(number, index) in numbers"  
-                    v-on:click="removeItem(index)"
-                    style="cursor: pointer"
-                    :key="number" >{{ number }}
-                </li>
-            </transition-group>
-           
+          <li class="list-group-item" v-for="(u, index) in users" :key="index">
+            <strong> Username:</strong>  {{ u.username }} - <br> <strong>Email</strong> {{ u.email }}
+          </li>
         </ul>
       </div>
     </div>
-    <hr />
   </div>
 </template>
 
 <script>
-// importing and registering the components
-import DangerAlert from './components/DangerAlert.vue'
-import SuccessAlert from './components/SuccessAlert'
-
-
 export default {
-    components: {
-        AppDangerAlert: DangerAlert,
-        AppSuccessAlert: SuccessAlert 
-
-    },
   data() {
     return {
-      show: false,
-      load: true,
-      alertAnimation: 'fade',
-      elementWidth: 100,
-      selectedComponent: 'app-success-alert',
-      numbers: [2, 4, 6, 8, 10, 12]
+      user: {
+        username: "",
+        email: ""
+      },
+      users: []
     };
   },
   methods: {
-      // animatin with JS
-      beforeEnter: function (el) {
-             console.log('beforeEnter');
-             this.elementWidth = 100;
-             el.style.width = this.elementWidth + 'px'
-      }, 
-      // es6
-      enter(el, done){
-          console.log('enter')
-          let round = 1;
-          const interval = setInterval(() => {
-              el.style.width = (this.elementWidth + round * 10) + 'px';
-              round++;
-              if(round > 20) {
-                  clearInterval(interval)
-                   done();
-              }
-              
-          }, 20);
-         
-      },
-      afterEnter(el){
-          console.log('afterEnter')
-      },
-      enterCancelled(el){
-          console.log('enterCancelled')
-      },
-      beforeLeave(el){
-          console.log('beforeLeave')
-          this.elementWidth = 300
-          el.style.width =  this.elementWidth + 'px'
-      },
-      leave(el, done){
-          console.log('leave')
-          let round = 1;
-          const interval = setInterval(() => {
-              el.style.width = (this.elementWidth - round * 10) + 'px';
-              round++;
-              if(round > 20) {
-                  clearInterval(interval)
-                   done();
-              }            
-          }, 20);
-      },
-      afterLeave(el){
-          console.log('afterLeave')
-      },
-      leaveCancelled(el){
-          console.log('leaveCancelled')
-      },
-      addItem(){
-          const pos = Math.floor(Math.random() * this.numbers.length);
-          this.numbers.splice(pos, 0, this.numbers.length + 1)
-      },
-      removeItem(index){
-          this.numbers.splice(index, 1)
-      }
+    // post data
+    submit() {
+      this.$http.post("",this.user).then(
+          response => {
+            console.log(response);
+          },
+          error => {
+            console.log(error);
+          }
+        );
+      // console.log(this.user);
+    },
+    // get/fetch data
+    fetchData: function() {
+      this.$http.get("").then(response => {
+          return response.json();
+          console.log(data);
+        })
+        .then(data => {
+          const resultArray = [];
+          for (let key in data) {
+            resultArray.push(data[key]);
+          }
+          this.users = resultArray;
+        });
+    }
   }
 };
 </script>
 
-<style>
-.fade-enter {
-  opacity: 0;
-}
-
-.fade-enter-active {
-  transition: opacity 1s;
-}
-
-.fade-leave {
-  /* opacity: 1; */
-}
-
-.fade-leave-active {
-  transition: opacity 1s;
-  opacity: 0;
-}
-
-.slide-enter {
-  /* transform: translateY(20px) */
-  opacity: 0;
-}
-
-.slide-enter-active {
-  animation: slide-in 1s ease-out forwards;
-  transition: opacity 0.5s;
-}
-.slide-leave {
-
-}
-.slide-leave-active {
-  animation: slide-out 1s ease-in-out forwards;
-  transition: opacity 1s;
-  opacity: 0;
-  position: absolute;
-}
-
-.style-move{
-    transition: transform 1s;
-}
-
-@keyframes slide-in {
-  from {
-    transform: translateY(20px);
-  }
-  to {
-    transform: translateY(0);
-  }
-}
-
-@keyframes slide-out {
-  from {
-    transform: translateY(0);
-  }
-  to {
-    transform: translateY(20px);
-  }
-}
-</style>
+<style></style>
--- vue-max-tuts/src/main.js
+++ vue-max-tuts/src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import VueResource from 'vue-resource'
 
+// use vue resourse 
+Vue.use(VueResource)
 
+// configuring vue resource globally
+Vue.http.options.root = 'https://max-http-request-default-rtdb.firebaseio.com./data.json'
+
+// intercepting requests 
+Vue.http.interceptors.push((request, next) => {
+     console.log(request);
+     if(request.method == 'POST'){
+      request.method == 'PUT'
+     }
+     next(response => {
+          response.join = () => { return {messages: response.body}}
+     });
+})
 
 new Vue({
   el: '#app',
